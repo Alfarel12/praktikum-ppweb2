8d847acf071fee0828428193b3ed909565232913
Alfarelputra <farelmuhammad113@gmail.com>
2025-03-03 08:11:34 +0700

    Add files via upload

---
+++ pertemuan 2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+ //mendefinisikan variable
+ $nama = "farel";
+ $umur = 20;
+ $bb = 70;
+
+ echo "Nama saya adalah : " .$nama;
+ echo "<br/>Umur : " .$umur. "Tahun";
+ echo "<br/>Berat badan : " .$bb. "Kg";
+?>
